0c8278252bbd5179f156baf60bbd0f9b5cade4a9
Rafael Hengles <rhengles@gmail.com>
2021-08-12 09:53:34 -0300

    feat(message): Add `raw` prop to `Message`

--- src/message/src/MessageEnvironment.tsx
+++ src/message/src/MessageEnvironment.tsx
@@ -98,6 +98,7 @@ export default defineComponent({
                 content={this.content}
                 type={this.type}
                 icon={this.icon}
+                raw={this.raw}
                 closable={this.closable}
                 onClose={this.handleClose}
                 onMouseenter={
